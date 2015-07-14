29c301af3dba41ef78025a029fe37ca36811ec4a
Amar Singh <reshubhan@gmail.com>
2015-07-14 06:14:05 +0000

    Added passport

--- app.js
+++ app.js
@@ -60,6 +60,26 @@ app.use(function(err, req, res, next) {
   });
 });
 
+var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
+MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+    if(err) throw err;
+
+    var collection = db.collection('test_insert');
+    collection.insert({a:2}, function(err, docs) {
+        collection.count(function(err, count) {
+            console.log(format("count = %s", count));
+        });
+    });
+
+    // Locate all the entries using find
+    collection.find().toArray(function(err, results) {
+        console.dir(results);
+        // Let's close the db
+        db.close();
+    });
+});
+
 app.listen(3000,function(){
 
   console.log('application running....')
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,59 @@ var cal = require('../app/calculations.js')
 var cal_controller = require('../app/controller/calculation_controller.js')
 var router = express.Router();
 var url = require('url');
+var passport = require('passport'),
+	LocalStrategy = require('passport-local').Strategy;
+
+
+var app = express();
+app.use(function(req, res, next){
+  // the status option, or res.statusCode = 404
+  // are equivalent, however with the option we
+  // get the "status" local available as well
+  res.render('404', { status: 404, url: req.url });
+});
+
+app.use(function(err, req, res, next){
+  // we may use properties of the error object
+  // here and next(err) appropriately, or if
+  // we possibly recovered from the error, simply next().
+  res.render('500', {
+      status: err.status || 500
+    , error: err
+  });
+});
+
+router.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/login',
+                                   failureFlash: true })
+);
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express Amar',name: "singh" });
 });
+
+/* GET home page. */
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard');
+});
+
 
 /* GET home page. */
 router.get('/cal', function(req, res, next) {
@@ -20,7 +68,7 @@ router.get('/amar', function(req, res, next) {
   	 res.render('amar', { result: data });
   });
 	
-	
+
 	
 	
   
